a1f0cf6c89b61b435fa73d9b6e633b4506ee6c8c
shubham-neewee <shubham.gour@neewee.ai>
2025-06-05 13:08:20 +0530

    Added Day-2 Learning

---
+++ day-2-variables-and-datatypes/first.py
@@ -0,0 +1,54 @@
+# Variable is a container where we store the values
+a = 1
+b = 2
+
+
+# Printing the integer value
+print("The integer value is : ",a)  # Output: 1
+print("The integer value is : ",b)  # Output: 2
+
+#Printing the addition of a and b taking the values as variable
+print("The Sum of the variables is : ",a+b)
+
+
+
+# Printing the name using the variable concept -- this is a string variable
+name = "Shubham"
+print("Name is ", name )
+
+
+# Using a decimal value as a variable which is called as float
+decimal_value = 10.5
+print("The Decimal value is : ",decimal_value)
+
+# Printing a boolean value
+is_admin = True
+print("The Boolean value is : ",is_admin)
+
+# Printing the none variable value -- we use this variable when we want to denote that there is nothing in the variable
+none_value = None
+print("The None value is : ",none_value)
+
+
+
+# Rules for defining the variables 
+# 1. The variable name should be a string of characters (a-z, A-Z,
+# 2. The variable name should not be a keyword in python
+# 3. The variable name should not start with a number
+# 4. The variable name should not contain any special characters except underscore
+# 5. The variable name should be case sensitive (i.e., it treats 'a' and 'A' as different characters)
+# 6. The variable name should not be too long (it should be less than 32 characters)
+# 7. The variable name should not be too short (it should be at least 1 character)
+# 8. The variable name should not contain any space
+# 9. The variable name should not contain any special characters except underscore
+# 10. The variable name should not be a reserved word in python
+# 11. The variable name should not be a special character
+# 12. The variable name should not be a number
+# 13. The variable name should not be a string of special characters
+# 14. The variable name should not be a string of numbers
+# 15. The variable name should not be a string of special characters and numbers
+
+
+
+
+
---
+++ day-2-variables-and-datatypes/second.py
@@ -0,0 +1,51 @@
+# Operators in Python
+# 1. Arithmetic Operators
+# 2. Comparison Operators
+# 3. Logical Operators
+# 4. Assignment Operators
+# 5. Bitwise Operators
+
+# when we write 7 + 4 = 11 here 7 and 4 are called operands and 11 is called results
+# 7 + 4 is called expression
+# 7 + 4 is called expression and + is called operator
+
+
+# 1. Arithmetic Operators
+a = 7
+b= 4 
+c = a + b
+print("The Sum of arithmetic operators : ",c)
+
+# Assignment Operator
+aa = 7 # assign 7 to aa
+print(aa)
+
+bb = 6-4 # assign 2 to bb (after calculation)
+print(bb)
+
+
+cc = 0
+cc += 3 # assign 5 to cc (after calculation)
+print(cc)
+
+
+# 2. Comparison Operators -- here the return value is always boolean
+dd = 7
+ee = 4
+print(dd > ee) # True
+print(dd < ee) # False
+
+# when we say >= it means greater than 'or' equal to
+print(dd >=ee) # True
+
+# when we say != it means not equal to
+print(dd != ee) # True
+
+# when we say == it means equal to
+print(dd == ee) # False
+
+
+# Logical Operators
+
+e = True or False
+print(e)
